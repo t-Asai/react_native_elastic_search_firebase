c4de8b3e20d116c27ccc4e66d8d3cad0c248dd62
t-Asai <takumi.a42@gmail.com>
2018-04-27 13:58:29 +0900

    add and modify files to install RNFirebase

---
+++ ios/Pods/Target Support Files/Pods-react_native_elastic_search_firebase/Pods-react_native_elastic_search_firebase-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_react_native_elastic_search_firebase : NSObject
+@end
+@implementation PodsDummy_Pods_react_native_elastic_search_firebase
+@end
